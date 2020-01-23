cf09cad553353808ebf168f061b6ce619ad7cca6
vagrant <vagrant@vagrant.vm>
2020-01-23 09:27:31 +0000

    apache server first commit

---
+++ recipes/server.rb
@@ -0,0 +1,19 @@
+#
+# Cookbook:: apache
+# Recipe:: server
+#
+# Copyright:: 2020, The Authors, All Rights Reserved.
+package 'apache2'  do
+	  action  :install
+end
+service 'apache2' do
+	  supports status: true
+	    action [:enable, :start]
+end
+file '/var/www/html/index.html' do
+	  content '<html>
+	    <body>
+	        <h1>hello world</h1>
+		  </body>
+		  </html>'
+end
